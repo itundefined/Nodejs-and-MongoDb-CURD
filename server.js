78684cd248021f60860f84dd74a569f8509ed26c
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-04 19:43:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 // // const articles = require('./Mongoose/mongoFunction');
 
-// app.use(express.json());
+app.use(express.json());
+const port = process.env.PORT || 3000
+console.log(`well the port is ${port}`);
 
-// app.listen(process.env.PORT || 3000;)
+// app.listen(port)
 
 // // app.use("/articles", articles);
 
