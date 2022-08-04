489323f6d101bf8e5fa29d9d50948e7bd9342136
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-04 19:40:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 // // const articles = require('./Mongoose/mongoFunction');
 
 // app.use(express.json());
