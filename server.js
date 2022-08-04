4e205004f88e88175f8166f033391c16a4f6a676
Tushar Sharma <tusharsharmaek@gmail.com>
2022-08-04 19:39:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,17 +11,16 @@
 // app.get("/", (req, res)=>{
 //     res.send("Temporary Route")
 // })
-// // const http = require('http');
-// // const port = process.env.PORT || 3000
+const http = require('http');
+const port = process.env.PORT || 3000
 
-// // const server = http.createServer((req, res) => {
-// //   res.statusCode = 200;
-// //   res.setHeader('Content-Type', 'text/html');
-// //   res.end('<h1>Hello World</h1>');
-// // });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
 
-// // server.listen(port,() => {
-// //   console.log(`Server running at port `+port);
-// // });
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
 
-console.log("this")
